93b1b71bfe229b114eab2a8152774bf9078be895
SHIRO-KUROTEN <johnny.vds@eica.be>
2024-11-01 19:10:42 +0000

    fini

--- Exercice_5.py
+++ Exercice_5.py
@@ -5,3 +5,11 @@
 #  Demandez à l'utilisateur d'entrer le mot de passe.
 #  Tant que le mot de passe n'est pas correct, redemandez-le (à l'aide d'une boucle while).
 #  Quand le mot de passe est correct, affichez "Accès autorisé".
+mdp="python123"
+x= input("entrer le mot de passe : ")
+
+while mdp != x :
+    
+    print("mot de passe incorect")
+    x= input("entrer le bon mot de passe : ")
+print(" pimp pam poum :","","le mot de passe correct")
